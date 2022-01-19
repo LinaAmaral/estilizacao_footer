fbd07ced25656b8c98576ffdeab7621cf820b950
LinaAmaral <lina.amaralcaetano@gmail.com>
2022-01-19 15:13:23 -0300

    estilização footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "../src/components/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="page-container">
+      <header className="content-wrap">
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
+        <h1>CONTENT FOR FOOTER</h1>
       </header>
+      <Footer />
     </div>
   );
 }
